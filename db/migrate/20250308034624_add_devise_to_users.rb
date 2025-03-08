08a6b479bf06f36e91b6018ff7d4f51efe1355be
clay <5711623+Clay26@users.noreply.github.com>
2025-03-07 23:08:18 -0500

    Update devise migration

--- db/migrate/20250308034624_add_devise_to_users.rb
+++ db/migrate/20250308034624_add_devise_to_users.rb
@@ -35,7 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[8.0]
       t.string :name
 
       # Uncomment below if timestamps were not included in your original model.
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true

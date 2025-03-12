ebbba27f121558cddb97ea16d6e6d95733605d02
clay <5711623+Clay26@users.noreply.github.com>
2025-03-12 16:14:36 -0400

    Highlight selected tab

--- app/javascript/controllers/workout_tab_controller.js
+++ app/javascript/controllers/workout_tab_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["tab"]
+
   connect() {
     this.element.addEventListener("turbo:frame-load", this.handleFrameLoad)
+    this.showActiveTab()
   }
 
   disconnect() {
@@ -14,5 +17,25 @@ export default class extends Controller {
     const newURL = frame.src
 
     history.pushState({}, "", newURL)
+
+    this.showActiveTab()
   }
+
+showActiveTab() {
+  const urlParams = new URLSearchParams(window.location.search)
+  const currentTab = urlParams.get("tab") || "completed"
+
+  this.tabTargets.forEach(link => {
+    const innerDiv = link.querySelector("div")
+    if (!innerDiv) {
+      return
+    }
+
+    if (link.href.includes(`tab=${currentTab}`)) {
+      innerDiv.classList.add("active")
+    } else {
+      innerDiv.classList.remove("active")
+    }
+  })
+}
 }

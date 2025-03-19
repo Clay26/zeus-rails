0ac08808752e357b3d2c2398701fa9e609430cfc
clay <5711623+Clay26@users.noreply.github.com>
2025-03-18 23:35:27 -0400

    Open and close drawer with esc + mouse clicks

--- app/javascript/controllers/drawer_controller.js
+++ app/javascript/controllers/drawer_controller.js
@@ -3,30 +3,44 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["drawer", "backdrop"]
 
-open() {
-  this.backdropTarget.classList.remove("hidden");
+  connect() {
+    document.addEventListener("keydown", this._handleKeydown);
+  }
 
-  requestAnimationFrame(() => {
-    this.backdropTarget.classList.remove("opacity-0");
-  });
+  disconnect() {
+    document.removeEventListener("keydown", this._handleKeydown);
+  }
 
-  this.drawerTarget.classList.remove("hidden");
- 
-  requestAnimationFrame(() => {
-    this.drawerTarget.classList.remove("translate-x-full");
-    this.drawerTarget.classList.add("translate-x-0");
-  });
-}
+  _handleKeydown = (event) => {
+    if (event.key === "Escape" && !this.drawerTarget.classList.contains("hidden")) {
+      this.close(); // Use your existing close logic
+    }
+  };
 
-close() {
-  this.drawerTarget.classList.remove("translate-x-0");
-  this.drawerTarget.classList.add("translate-x-full");
+  open() {
+    this.backdropTarget.classList.remove("hidden");
 
-  this.backdropTarget.classList.add("opacity-0");
+    requestAnimationFrame(() => {
+      this.backdropTarget.classList.remove("opacity-0");
+    });
 
-  this.drawerTarget.addEventListener("transitionend", () => {
-    this.drawerTarget.classList.add("hidden");
-    this.backdropTarget.classList.add("hidden");
-  }, { once: true });
-}
+    this.drawerTarget.classList.remove("hidden");
+
+    requestAnimationFrame(() => {
+      this.drawerTarget.classList.remove("translate-x-full");
+      this.drawerTarget.classList.add("translate-x-0");
+    });
+  }
+
+  close() {
+    this.drawerTarget.classList.remove("translate-x-0");
+    this.drawerTarget.classList.add("translate-x-full");
+
+    this.backdropTarget.classList.add("opacity-0");
+
+    this.drawerTarget.addEventListener("transitionend", () => {
+      this.drawerTarget.classList.add("hidden");
+      this.backdropTarget.classList.add("hidden");
+    }, { once: true });
+  }
 }
